84c3ee66e3b91af52589bab39abf312c31dc1771
Rene Melendez <rene.melendeza@gmail.com>
2019-05-27 21:07:23 -0600

    clientes

--- src/interfaces/Ruedas.java
+++ src/interfaces/Ruedas.java
@@ -1,7 +1,15 @@
 package interfaces;
 
 
+import java.util.ArrayList;
+
 public interface Ruedas{
+     int precio=0;
+     ArrayList<>
+     void AnadirPersonas();
+     void MostrarAsientos();
+     int MostrarGanancias();
+
 
 
 }
