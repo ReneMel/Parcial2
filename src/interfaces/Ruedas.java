031ec701f26d6b1fb04064c12fbd695116ee388f
Rene Melendez <rene.melendeza@gmail.com>
2019-05-27 21:08:41 -0600

    clientes2

--- src/interfaces/Ruedas.java
+++ src/interfaces/Ruedas.java
@@ -1,11 +1,13 @@
 package interfaces;
 
 
+import Clases.Cliente;
+
 import java.util.ArrayList;
 
 public interface Ruedas{
      int precio=0;
-     ArrayList<>
+     ArrayList<Cliente> Pasajeros= new ArrayList<>();
      void AnadirPersonas();
      void MostrarAsientos();
      int MostrarGanancias();
